577da3daf2c2267bf57086b22879b1bee99bf982
depinfo <papajr@outlook.fr>
2018-05-26 01:34:15 +0200

    Model + controller event

--- controller/ControllerEvent.php
+++ controller/ControllerEvent.php
@@ -7,167 +7,60 @@ class ControllerEvent { //TODO: tester chaque variable POST, GET et SESSION avan
 
     protected static $object = 'event';
 
-    public static function readAll(){
-        if(Session::is_user()){
-            $tab_v = ModelUser::selectAll();     //appel au modèle pour gerer la BD
+    public static function readAll()
+    {
+        if (Session::is_user()) {
+            $tab_v = ModelEvent::selectAll();     //appel au modèle pour gerer la BD
             //"redirige" vers la vue (pas require_once car on peut appeler plusieur fois dans le code pour 'copier' le html à la manière d'un include en C
-            $pagetitle = 'List';
-            require File::build_path(array('view', 'user', 'List.php'));
+            $pagetitle = 'List Evenements';
+            require File::build_path(array('view', 'event', 'List.php'));
         } else {
             ControllerMain::error();
         }
     }
 
-    public static function read() {
-        if(isset($_GET['login']) && (Session::is_user($_GET['login'])||Session::is_admin())){
-            $login=$_GET['login'];
-            if(!$tv = ModelUser::select(array('login'=>$login))){
-                ControllerMain::error();
-            } else {
-                $v = $tv[0];
-                $pagetitle = 'DetailUser';
-                require File::build_path(array('view', 'user', 'Detail.php'));
-            }
-        } else {
-            ControllerMain::error();
-        }
-    }
-
-    public static function create() {
-        $pagetitle='Create';
-        require File::build_path(array('view', 'user', 'Create.php'));
-    }
-
-    public static function created() {
-        $error=[];
-        $data=array(
-            'login'=>$_POST['login'],
-            'nonce'=>Security::generateRandomHex(),
-            'nom'=>$_POST['nom'],
-            'prenom'=>$_POST['prenom'],
-            'isAdmin'=>0);
-        if($_POST['mdp']==$_POST['mdp2']){
-            $mdp= Security::getSeed().$_POST['mdp'];
-            $data['mdp']= Security::chiffrer($mdp);
-        }
-        sleep(3);
-        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $data['email']=$_POST['email'];
-        }
-        $mail="Valider votre email à l'adresse: http://localhost/Web/index.php?action=validate&page=user&login=".$_POST['login']."&nonce=".$data['nonce'];
-        mail($_POST['email'], 'Validation création de compte sur Concours', $mail);
-        if(!ModelUser::select(array('login'=>$data['login']))){
-            if(!ModelUser::save($data)){ //NULL est interprété comme non vrai aussi donc soit on return true en cas de succès soit on teste si $car1->save() === false (le === check si c'est bien un boolean et si c'est false donc si c'est NULL ça ne sera pas un boolean)
-                ControllerMain::error();
-            } else {
-
-                $tab_v = ModelUser::selectAll();
-                $pagetitle='Accueil';
-                require File::build_path(array('view', 'user', 'Created.php'));
-            }
-        }else{
-            array_push($error, 'Le login existe déjà');
-            echo json_encode($error);
+    public static function read()
+    {
+        //read the Model
+        if (isset($_GET['id'])) {
+            $tab_v = ModelEvent::search($_GET['id']);
+            $v = $tab_v[0];
+            $pagetitle = 'Detail de l\'evenements';
+            require File::build_path(array('view', 'event', 'Detail.php'));
         }
     }
 
     public static function update() {
-        if(isset($_GET['login']) && (Session::is_user($_GET['login'])||Session::is_admin())){
-            $tv=ModelUser::select(array('login'=>$_GET['login']));
+        $pagetitle='Create';
+        require File::build_path(array('view', 'event', 'Update.php'));
+    }
+
+    public static function updated(){
+
+        if($_POST['public'] == NULL){
+            $public = 0;
+        }else{
+            $public = 1;
+        }
+        $data=array(
+            'localisation'=>$_POST['localisation'],
+            'titre'=>$_POST['titre'],
+            'description'=>$_POST['description'],
+            'date'=>$_POST['date'],
+            'heure'=>$_POST['heure'] + ":" + $_POST['minutes'],
+            'public'=>$public,
+            'loginCreateur'=>$_POST['loginCreateur']);
+        var_dump($data);
+        if (!ModelEvent::save($data)) { //NULL est interprété comme non vrai aussi donc soit on return true en cas de succès soit on teste si $car1->save() === false (le === check si c'est bien un boolean et si c'est false donc si c'est NULL ça ne sera pas un boolean)
+            ControllerMain::error();
+        } else {
+            $tv = ModelEvent::select(array('id'=>$_POST['id']));
             $v=$tv[0];
-            $pagetitle='Update';
-            require File::build_path(array('view', 'user', 'Update.php'));
-        }else{
-            ControllerMain::error();
+            $pagetitle='Accueil';
+            require File::build_path(array('view', 'event', 'Updated.php'));
         }
     }
 
-    public static function updated() {
-        if(isset($_GET['login']) && (Session::is_user($_GET['login'])||Session::is_admin())){
-            $data=array(
-                'login'=>$_POST['login'],
-                'nom'=>$_POST['nom'],
-                'prenom'=>$_POST['prenom']);
-            if($_POST['mdp']==$_POST['mdp2']){
-                $mdp= Security::getSeed().$_POST['mdp'];
-                $data['mdp']= Security::chiffrer($mdp);
-            }
-            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                $data['email'] = $_POST['email'];
-            }
-            if (Session::is_admin() && isset($_POST['isAdmin'])) {
-                $data['isAdmin'] = 1;
-            }else{
-                $data['isAdmin'] = 0;
-            }
-            if (!ModelUser::update($data)) { //NULL est interprété comme non vrai aussi donc soit on return true en cas de succès soit on teste si $car1->save() === false (le === check si c'est bien un boolean et si c'est false donc si c'est NULL ça ne sera pas un boolean)
-                ControllerMain::error();
-            } else {
-                $tv = ModelUser::select(array('login'=>$_POST['login']));
-                $v=$tv[0];
-                $pagetitle='Accueil';
-                require File::build_path(array('view', 'user', 'Updated.php'));
-            }
-        } else {
-            ControllerMain::error();
-        }
-    }
 
-    public static function delete() {
-        if(isset($_GET['login']) && (Session::is_user($_GET['login'])||Session::is_admin())){
-            ModelUser::delete(array('login'=>$_GET['login']));
-            if(Session::is_user($_GET['login'])) {
-                session_unset();
-                session_destroy();
-            }
-            $pagetitle = 'List';
-            require File::build_path(array('view', 'user', 'Deleted.php'));
-        } else {
-            ControllerMain::error();
-        }
-    }
 
-    public static function connect(){
-        $pagetitle = 'Connexion';
-        require File::build_path(array('view', 'user', 'Connect.php'));
-    }
-
-    public static function connected() {
-        if(isset($_POST['login']) && isset($_POST['mdp'])){
-            $mdp1=Security::getSeed().$_POST['mdp'];
-            $mdp= Security::chiffrer($mdp1);
-            if(ModelUser::checkPassword($_POST['login'], $mdp)&&ModelUser::isValide($_POST['login'])){
-                $_SESSION['login']=$_POST['login'];
-                $tv = ModelUser::select(array('login'=>$_POST['login']));
-                $v=$tv[0];
-                if($v->isAdmin()){
-                    $_SESSION['admin']=true;
-                }
-                require File::build_path(array('view', 'user', 'Connected.php'));
-            }else{
-                ControllerMain::error();
-            }
-        }else{
-            ControllerMain::error();
-        }
-    }
-
-    public static function deconnect(){
-        session_unset();
-        session_destroy();
-        ControllerMain::accueil();
-    }
-
-    public static function validate(){
-        if(isset($_GET['login'])){
-            $login=$_GET['login'];
-            $tv = ModelUser::select(array('login'=>$login));
-            if(isset($_GET['nonce']) && $tv[0]->get("nonce")==$_GET["nonce"]){
-                ModelUser::validate($login);
-            }
-            ControllerMain::accueil();
-        }else{
-            ControllerMain::error();
-        }
-    }
 }
--- model/ModelEvent.php
+++ model/ModelEvent.php
@@ -4,34 +4,64 @@ require_once File::build_path(array('model','Model.php'));
 class ModelEvent extends Model
 {
     private $id;
-    private $adresse;
+    private $hash;
+    private $localisation;
     private $titre;
     private $description;
     private $date;
     private $heure;
     private $loginCreateur;
     private $public;
-    protected static $object='event';
-    protected static $primary='id';
+    protected static $object = 'event';
+    protected static $primary = 'id';
 
-    public function __construct($id=NULL, $adresse=NULL, $titre=NULL, $description=NULL, $date=NULL, $heure=NULL, $loginCreateur=NULL, $public=NULL)
-{
-    if (!is_null($id) && !is_null($adresse) && !is_null($titre) && !is_null($description) && !is_null($date) && !is_null($heure) && !is_null($loginCreateur) && !is_null($public)) {
-        $this->id = $id;
-        $this->adresse = $adresse;
-        $this->titre = $titre;
-        $this->description = $description;
-        $this->date = $date;
-        $this->heure = $heure;
-        $this->loginCreateur = $loginCreateur;
-        $this->public = $public;
+    public function __construct($id = NULL, $localisation = NULL, $titre = NULL, $description = NULL, $date = NULL, $heure = NULL, $loginCreateur = NULL, $public = NULL)
+    {
+        if (!is_null($id) && !is_null($localisation) && !is_null($titre) && !is_null($description) && !is_null($date) && !is_null($heure) && !is_null($loginCreateur) && !is_null($public)) {
+            $this->id = $id;
+            $this->localisation = $localisation;
+            $this->titre = $titre;
+            $this->description = $description;
+            $this->date = $date;
+            $this->heure = $heure;
+            $this->loginCreateur = $loginCreateur;
+            $this->public = $public;
+        }
+    }
+
+    // getter
+    public function get($attribut)
+    {
+        return $this->$attribut;
+    }
+
+    public static function search($data){
+        $sql = "SELECT * FROM Evenement e WHERE e.id:=id";
+        // Prepare the SQL statement
+        $req_prep = Model::$pdo->prepare($sql);
+        // Execute the SQL prepared statement after replacing tags
+        $req_prep->execute($data); //on associe le tableau à la requete pour éviter l'injection
+        // Retrieve results as previously
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelBiere');
+        $tab = $req_prep->fetchAll();
+        return $tab;
+    }
+
+    public static function save($data)  #Va d'abord chercher l'id max dans la BD puis l'incrémente et en fait un hash
+    {
+        try {
+            $sql = 'SELECT MAX(id) FROM Event';
+            $req_prep = Model::$pdo->prepare($sql);
+            $req_prep->execute($data);
+            $lastId = $req_prep->fetchColumn();
+        } catch (PDOException $e) {
+            echo $e->getMessage(); // affiche un message d'erreur
+        }
+        $newId = $lastId +  1;
+        $newData = $data;
+        $newData['id'] = $newId;
+        $newData['hash'] = Security::chiffrer($newId);
+        return parent::save($newData);
     }
 }
 
-
-    // getter
-    public function get($attribut){
-    return $this->$attribut;
-}
-
-}
