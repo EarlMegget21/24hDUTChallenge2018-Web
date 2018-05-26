c4365d478d58b95995b4da87c8a2e34cb1de23a4
Nicolas Le Gallo <nico30.legallo@gmail.com>
2018-05-26 02:03:39 +0200

    log-in + sign-up

--- view/user/Connect.php
+++ view/user/Connect.php
@@ -1,32 +1,19 @@
-<form method="post" id="signinForm" class="form" action="/Web/index.php?action=connected&controller=user">
-    <!-- action determine le fichier dans lequel on est redirigé avec les variables rentrées après Submit -->
-    <h3>Connexion:</h3>
-    <label for="login_id">Login</label>
-    <input type="text" name="login" id="login_id" required/>
 
-    <label for="mdpConnect">Mot de passe</label>
-    <input type="password" name="mdp" id="mdpConnect" required/>
-
-    <input class="submitButton" type="submit" value="Se Connecter"/>
-</form>
-<div class="mdc-card" id="logincard">
+<form class="mdc-card" id="logincard">
   <div class="mdc-typography--title" id="title">Connexion</div>
-  <div class="mdc-text-field">
-    <input type="text" id="identifiant" class="mdc-text-field__input">
-    <label class="mdc-floating-label" for="identifiant">Identifiant</label>
+  <div class="mdc-text-field" id="identifiant">
+    <input type="text" id="login_id" name="login" class="mdc-text-field__input">
+    <label class="mdc-floating-label" for="login_id">Identifiant</label>
     <div class="mdc-line-ripple"></div>
   </div>
-  <div class="mdc-text-field">
-    <input type="password" id="password" class="mdc-text-field__input">
-    <label class="mdc-floating-label" for="password">Password</label>
+  <div class="mdc-text-field" id="password">
+    <input type="password" id="mdpConnect" name="mdp" class="mdc-text-field__input">
+    <label class="mdc-floating-label" for="mdpConnect">Password</label>
     <div class="mdc-line-ripple"></div>
   </div>
-  <button class="mdc-fab material-icons" aria-label="Favorite">
-    <span class="mdc-fab__icon">
-      favorite
-    </span>
-  </button>
-</div>
+  <input id="logbutton" class="mdc-fab material-icons" type="submit" aria-label="Favorite">
+  </input>
+</form>
 
 
 <style>
@@ -42,3 +29,14 @@
 
 
 <script src="/Web/js/users.js" async defer></script>
+<script>
+  const buttonRipple = new mdc.ripple.MDCRipple(document.querySelector('#logbutton'));
+ </script>
+ <script>
+   const textField = new mdc.textField.MDCTextField(document.querySelector('#identifiant'));
+   const textFieldPass = new mdc.textField.MDCTextField(document.querySelector('#password'));
+ </script>
+
+ <?php
+     require File::build_path(array('view','user','Create.php'));
+ ?>
