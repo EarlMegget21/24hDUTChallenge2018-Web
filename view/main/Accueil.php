6a52f18c02c97f0b7e552d41f224fcc782eb03d5
Unknown <at.frizzy@gmail.com>
2018-05-26 05:25:29 +0200

    rectif

--- view/main/Accueil.php
+++ view/main/Accueil.php
@@ -5,8 +5,14 @@
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean faucibus dignissim viverra. Curabitur eu rhoncus massa. Nulla vel tortor vestibulum, rutrum purus eu, rutrum neque. Mauris eget tempor mauris. Vivamus quis feugiat lectus. Pellentesque condimentum accumsan odio, vitae pretium erat mollis id. Phasellus condimentum turpis sit amet ligula gravida tincidunt.
     </section>
     <section class="mdc-card__actions">
-        <a target="_blank" href="Web/view/event/Update.php" class="mdc-button mdc-button--compact mdc-card__action mdc-ripple">
+        <a href="#" id="creerEvent" class="mdc-button mdc-button--compact mdc-card__action mdc-ripple">
             <i class="mdc-button__icon material-icons">add</i>
             Créer un évenement</a>
     </section>
 </div>
+
+<script>
+    $('#creerEvent').click(function(e) {
+        changePage({'controller':"event","action":"update"},"CreateEvenement")
+    })
+</script>
